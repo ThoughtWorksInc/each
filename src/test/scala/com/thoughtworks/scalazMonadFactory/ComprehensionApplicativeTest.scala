2d3fea48adde36c2b1f03a08f63168cb6ef9f1a0
Yang Bo <atryyang@thoughtworks.com>
2015-07-08 18:29:17 +0800

    Use assertEquals instead of assertTrue

--- src/test/scala/com/thoughtworks/scalazMonadFactory/ComprehensionApplicativeTest.scala
+++ src/test/scala/com/thoughtworks/scalazMonadFactory/ComprehensionApplicativeTest.scala
@@ -7,13 +7,13 @@ class ComprehensionApplicativeTest {
   @Test
   def testSeqPoint(): Unit = {
     val seqApplicative = ComprehensionApplicative[Seq]
-    Assert.assertTrue(Seq("hello, applicative") == seqApplicative.point("hello, applicative"))
+    Assert.assertEquals(Seq("hello, applicative"), seqApplicative.point("hello, applicative"))
   }
 
   @Test
   def testSeqAp(): Unit = {
     val seqApplicative = ComprehensionApplicative[Seq]
-    Assert.assertTrue(Seq("Hello1!", "Hello1?", "Hello2!", "Hello2?") == seqApplicative.ap(Seq("Hello1", "Hello2"))(Seq((_: String) + "!", (_: String) + "?")))
+    Assert.assertEquals(Seq("Hello1!", "Hello1?", "Hello2!", "Hello2?"), seqApplicative.ap(Seq("Hello1", "Hello2"))(Seq((_: String) + "!", (_: String) + "?")))
   }
 
 }

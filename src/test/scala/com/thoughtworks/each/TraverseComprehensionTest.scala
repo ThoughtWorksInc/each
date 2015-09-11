2f9f9e636acd1c77cc16736f6d328a45afe94ea2
Yang Bo <atryyang@thoughtworks.com>
2015-09-12 01:42:46 +0800

    Use List in test cases

--- src/test/scala/com/thoughtworks/each/TraverseComprehensionTest.scala
+++ src/test/scala/com/thoughtworks/each/TraverseComprehensionTest.scala
@@ -2,9 +2,8 @@ package com.thoughtworks.each
 
 import org.junit.{Assert, Test}
 import Monadic._
-import scalaz.syntax.traverse._
 import scalaz.std.option._
-import scalaz.std.iterable._
+import scalaz.std.list._
 
 class TraverseComprehensionTest {
 
@@ -13,7 +12,7 @@ class TraverseComprehensionTest {
     val n = Some(10)
     val result = monadic[Option] {
       var count = 1
-      for (i <- Iterable(300, 20).monadicLoop) {
+      for (i <- List(300, 20).monadicLoop) {
         count += i * n.each
       }
       count
@@ -25,22 +24,22 @@ class TraverseComprehensionTest {
   def testMap(): Unit = {
     val n = Some(4000)
     val result = monadic[Option] {
-      (for (i <- Iterable(300, 20).toEphemeralStream.monadicLoop) yield {
+      (for (i <- List(300, 20).monadicLoop) yield {
         i + n.each
-      }).to[Iterable]
+      }).underlying
     }
-    Assert.assertEquals(Some(Iterable(4300, 4020)), result)
+    Assert.assertEquals(Some(List(4300, 4020)), result)
   }
 
 
   @Test
   def testMapWithAnotherType(): Unit = {
     val result = monadic[Option] {
-      (for (i <- Iterable("foo", "bar-baz").toEphemeralStream.monadicLoop) yield {
+      (for (i <- List("foo", "bar-baz").monadicLoop) yield {
         i.length
-      }).to[Iterable]
+      }).underlying
     }
-    Assert.assertEquals(Some(Iterable(3, 7)), result)
+    Assert.assertEquals(Some(List(3, 7)), result)
   }
 
   @Test
@@ -48,13 +47,13 @@ class TraverseComprehensionTest {
     val n = Some(4000)
     val result = monadic[Option] {
       (for {
-        i <- Iterable(300, 20).toEphemeralStream.monadicLoop
-        j <- Iterable(50000, 600000).toEphemeralStream.monadicLoop
+        i <- List(300, 20).monadicLoop
+        j <- List(50000, 600000).monadicLoop
       } yield {
           i + j + n.each
-        }).to[Iterable]
+        }).underlying
     }
-    Assert.assertEquals(Some(Iterable(54300, 604300, 54020, 604020)), result)
+    Assert.assertEquals(Some(List(54300, 604300, 54020, 604020)), result)
   }
 
   @Test
@@ -63,13 +62,13 @@ class TraverseComprehensionTest {
 
     val result = monadic[Option] {
       (for {
-        i <- Iterable(300, 20).toEphemeralStream.monadicLoop
+        i <- List(300, 20).monadicLoop
         if i > 100
       } yield {
           i + n.each
-        }).to[Iterable]
+        }).underlying
     }
-    Assert.assertEquals(Some(Iterable(4300)), result)
+    Assert.assertEquals(Some(List(4300)), result)
   }
 
   @Test
@@ -77,16 +76,16 @@ class TraverseComprehensionTest {
     val n = Some(4000)
     val result = monadic[Option] {
       (for {
-        i <- Iterable(300, 20).toEphemeralStream.monadicLoop
-        (j, k) <- Iterable(50000 -> "1111", 600000 -> "yyy").toEphemeralStream.monadicLoop
+        i <- List(300, 20).monadicLoop
+        (j, k) <- List(50000 -> "1111", 600000 -> "yyy").monadicLoop
         if i > n.each - 3900
         a = i + j
       } yield {
           a + n.each * k.length
-        }).to[Iterable]
+        }).underlying
     }
 
-    Assert.assertEquals(Some(Iterable(66300, 612300)), result)
+    Assert.assertEquals(Some(List(66300, 612300)), result)
   }
 
 }

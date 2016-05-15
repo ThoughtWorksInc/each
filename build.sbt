2569104a061ada1178e7f47da0311a716d35d453
Yang Bo <atryyang@thoughtworks.com>
2016-05-16 02:55:31 +0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.2.2"
+
+libraryDependencies += "com.thoughtworks.each" %% "each" % "0.6.0"
+
+scalaVersion := "2.11.8"
---
+++ src/main/scala/com/thoughtworks/generator.scala
@@ -0,0 +1,88 @@
+package com.thoughtworks
+
+import scala.collection.generic.SeqFactory
+
+
+import scala.annotation.{StaticAnnotation,  tailrec}
+import scala.collection.GenSeq
+import scala.collection.mutable.LazyBuilder
+import scala.collection.mutable.ListBuffer
+import scala.collection.LinearSeqOptimized
+import scala.collection.LinearSeq
+import scala.collection.generic.GenericTraversableTemplate
+import scala.collection.generic.SeqFactory
+
+import scalaz.Free
+import scalaz.\/
+import scalaz.-\/
+import scalaz.std.list._
+import scalaz.std.tuple._
+import com.thoughtworks.each.Monadic._
+/**
+  * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
+  */
+class generator {
+
+}
+
+object generator {
+
+
+  object Generator extends SeqFactory[Generator] {
+
+    override final def newBuilder[Element] = {
+      List.newBuilder[Element].mapResult { upstream =>
+        type BuildingSource[A] = Free.Source[Element, A]
+        val buildingSource = monadic[BuildingSource] {
+          for (element <- upstream.monadicLoop) {
+            (Free.produce(element): BuildingSource[Unit]).each
+          }
+        }
+        freeSourceToGenerator(buildingSource)
+      }
+    }
+
+    implicit def freeSourceToGenerator[A](freeSource: Free.Source[A, Unit]) = {
+      freeSource.resume match {
+        case -\/((head, tailSource)) => NonEmpty(head, tailSource)
+        case _ => Empty
+      }
+    }
+
+    override final def empty[A] = Empty
+
+    private[Generator] final case object Empty extends Generator[Nothing] {
+
+      override final def isEmpty: Boolean = true
+
+      override final def head: Nothing = {
+        throw new NoSuchElementException("head of empty list")
+      }
+
+      override final def tail: Nothing = {
+        throw new UnsupportedOperationException("tail of empty list")
+      }
+
+    }
+
+    private[Generator] final case class NonEmpty[A](override val head: A, tailSource: Free.Source[A, Unit]) extends Generator[A] {
+
+      override final def isEmpty: Boolean = false
+
+      override final def tail: Generator[A] = {
+        freeSourceToGenerator(tailSource)
+      }
+
+    }
+  }
+
+  sealed abstract class Generator[+A]
+    extends scala.collection.immutable.Seq[A]
+      with scala.collection.immutable.LinearSeq[A]
+      with GenericTraversableTemplate[A, Generator]
+      with LinearSeqOptimized[A, Generator[A]] {
+
+    override final def companion = Generator
+
+  }
+}

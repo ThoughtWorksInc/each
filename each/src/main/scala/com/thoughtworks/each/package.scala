bd7e4c87398e15da961a4820deeb06d692f353b9
Scala Steward <scala_steward@virtuslab.com>
2023-03-28 20:19:33 +0000

    Reformat with scalafmt 3.7.3
    Executed command: scalafmt --non-interactive

--- each/src/main/scala/com/thoughtworks/each/package.scala
+++ each/src/main/scala/com/thoughtworks/each/package.scala
@@ -1,12 +1,12 @@
 package com.thoughtworks
 
-/**
-  * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
+/** @author
+  *   杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
 package object each {
 
-  /**
-    * Contains implicit methods to work with types that support `for`/`yield` comprehension.
+  /** Contains implicit methods to work with types that support `for`/`yield`
+    * comprehension.
     */
   val ComprehensionImplicits = sde.comprehensionMonad.ComprehensionMonad
 

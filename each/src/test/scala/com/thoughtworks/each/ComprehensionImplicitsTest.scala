59a8ab17f625c20e1a1494e12501fbaefe2716a0
Scala Steward <scala_steward@virtuslab.com>
2023-06-01 03:15:50 +0000

    Reformat with scalafmt 3.7.4
    Executed command: scalafmt --non-interactive

--- each/src/test/scala/com/thoughtworks/each/ComprehensionImplicitsTest.scala
+++ each/src/test/scala/com/thoughtworks/each/ComprehensionImplicitsTest.scala
@@ -12,7 +12,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+ */
 
 package com.thoughtworks.each
 
@@ -23,28 +23,41 @@ class ComprehensionImplicitsTest {
   @Test
   def testListPoint(): Unit = {
     val seqApplicative = ComprehensionImplicits.comprehensionMonad[List]
-    Assert.assertEquals(List("hello, applicative"), seqApplicative.point("hello, applicative"))
+    Assert.assertEquals(
+      List("hello, applicative"),
+      seqApplicative.point("hello, applicative")
+    )
   }
 
   @Test
   def testSeqPoint(): Unit = {
     val seqApplicative = ComprehensionImplicits.comprehensionMonad[Seq]
-    Assert.assertEquals(Seq("hello, applicative"), seqApplicative.point("hello, applicative"))
+    Assert.assertEquals(
+      Seq("hello, applicative"),
+      seqApplicative.point("hello, applicative")
+    )
   }
 
   @Test
   def testSeqAp(): Unit = {
     val seqApplicative = ComprehensionImplicits.comprehensionMonad[Seq]
-    Assert.assertEquals(Seq("Hello1!", "Hello2!", "Hello1?", "Hello2?"), seqApplicative.ap(Seq("Hello1", "Hello2"))(Seq((_: String) + "!", (_: String) + "?")))
+    Assert.assertEquals(
+      Seq("Hello1!", "Hello2!", "Hello1?", "Hello2?"),
+      seqApplicative.ap(Seq("Hello1", "Hello2"))(
+        Seq((_: String) + "!", (_: String) + "?")
+      )
+    )
   }
 
   @Test
   def testOptionMap(): Unit = {
     val optionBind = ComprehensionImplicits.comprehensionMonad[Option]
     Assert.assertEquals(
-      Option("hello, applicative"), optionBind.map(Option("hello, ")) { a =>
+      Option("hello, applicative"),
+      optionBind.map(Option("hello, ")) { a =>
         a + "applicative"
-      })
+      }
+    )
   }
 
   @Test
@@ -54,18 +67,20 @@ class ComprehensionImplicitsTest {
       Option("hello, applicative"),
       optionBind.bind(Option("hello, ")) { a =>
         Option(a + "applicative")
-      })
+      }
+    )
   }
 
   @Test
   def testSeqMap(): Unit = {
     val seqBind = ComprehensionImplicits.comprehensionMonad[Seq]
     Assert.assertEquals(
-      Seq("hello, applicative"), seqBind.map(Seq("hello, ")) { a =>
+      Seq("hello, applicative"),
+      seqBind.map(Seq("hello, ")) { a =>
         a + "applicative"
-      })
+      }
+    )
   }
-
 
   @Test
   def testSeqBind(): Unit = {
@@ -74,7 +89,8 @@ class ComprehensionImplicitsTest {
       Seq("hello, applicative"),
       seqBind.bind(Seq("hello, ")) { a =>
         Seq(a + "applicative")
-      })
+      }
+    )
   }
 
 }
